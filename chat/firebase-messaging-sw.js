8fa0c60fd54ad4cb24feb7559f00921fd6aa77e3
IdiotsandInnovation <148596832+IdiotsandInnovation@users.noreply.github.com>
2025-12-21 15:35:10 -0800

    Update Firebase messaging service worker configuration

--- chat/firebase-messaging-sw.js
+++ chat/firebase-messaging-sw.js
@@ -1,22 +1,22 @@
-importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyA9GsP72R_43JW7rG8Dpvcvt_n2FbQe5SM",
+  authDomain: "bobbyreichert-chat.firebaseapp.com",
+  projectId: "bobbyreichert-chat",
+  messagingSenderId: "743766854322",
+  appId: "1:743766854322:web:a492e9eb137d903a7b17ab"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(payload => {
+messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(
     payload.notification.title,
     {
       body: payload.notification.body,
-      icon: payload.notification.icon || '/icon.png'
+      icon: payload.notification.icon
     }
   );
 });
